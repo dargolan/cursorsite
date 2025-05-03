d2e368acbbc91f43afdede36895e34bc7216b0ec
dargolan <dargolan@gmail.com>
2025-05-03 19:01:41 +0200

    Remove stem and waveform components

--- src/components/AudioPlayer/index.ts
+++ src/components/AudioPlayer/index.ts
@@ -3,5 +3,3 @@ export { default } from './AudioPlayer';
 
 // Export subcomponents
 export { default as PlayButton } from './PlayButton';
-export { default as WaveformVisualizer } from './WaveformVisualizer';
-export { default as StemPlayer } from './StemPlayer'; 
